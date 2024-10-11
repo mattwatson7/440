68f3cb15a33f07c6a8fab9ee4a20861e987e5d71
Matthew Watson <113383737+mattwatson7@users.noreply.github.com>
2024-10-10 23:15:01 -0500

    Update main.ml

--- hw3/hw3-src/src/main.ml
+++ hw3/hw3-src/src/main.ml
@@ -50,10 +50,10 @@ let rec step (w:expr) : expr = match w with
 | Let(x, e1, e2) -> if(isval e1) then (subst x e1 e2) else Let(x, step e1, e2)
 | If(e, e1, e2) -> if(e=Boolean(true)) then e1 else (if(e=Boolean(false)) then e2 else If(step e, e1, e2))
 | Fun _ -> raise CannotStep
-| App(e1, e2) -> Nil
+| App _ -> Nil
 | Nil -> raise CannotStep
 | Cons(e1, e2) -> if(isval e1) then (if(isval e2) then Cons(e1, e2) else Cons(e1, step e2)) else Cons(step e1, e2)
-| Match(e1, e2, x, y, e3) -> Nil
+| Match _ -> Nil
 
 let rec stepstar (e:expr) : expr = 
 if (isval e) then e else (stepstar (step e))
