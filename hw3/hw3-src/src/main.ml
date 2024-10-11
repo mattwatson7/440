47be3f7a8178eef41ae74804f1a1f804d2a50af8
Matthew Watson <113383737+mattwatson7@users.noreply.github.com>
2024-10-10 22:47:59 -0500

    Update main.ml

--- hw3/hw3-src/src/main.ml
+++ hw3/hw3-src/src/main.ml
@@ -9,13 +9,13 @@ exception CannotStep
 
 let parse (s:string) : expr =
 let lexbuf = Lexing.from_string s in
-let ast = Parser.prog Lexer.read lexbuf in 
-ast
+let ast = Parser.prog Lexer.read lexbuf in ast
 
 
 let isval (e:expr) : bool = match e with 
 | Integer _ -> true
 | Boolean _ -> true
+| Fun _ -> true
 | _ -> false
 
 
@@ -35,6 +35,11 @@ let rec subst x (e1:expr) (e2:expr) = match e2 with
 | Binop(bop, e3, e4) -> Binop (bop, (subst x e1 e3),(subst x e1 e4)) 
 | Let(y, e3, e4) -> if y=x then Let(y, (subst x e1 e3), e4) else Let(y, (subst x e1 e3), (subst x e1 e4))
 | If(e3, e4, e5) ->  If((subst x e1 e3), (subst x e1 e4), (subst x e1 e5))
+| Fun(y, e) -> if y = x then Fun(y, e) else Fun(y, subst x e1 e)
+| App(e3, e4) -> App(subst x e1 e3, subst x e1 e4)
+| Nil -> Nil
+| Cons(e3, e4) -> Cons(subst x e1 e3, subst x e1 e4)
+| Match(e3, e4, y, z, e5) -> Match(subst x e1 e3, subst x e1 e4, y, z, subst x e1 e5)
 | _ -> raise CannotStep
 
 
@@ -45,6 +50,11 @@ let rec step (w:expr) : expr = match w with
 | Binop (bop, e1, e2) -> if(isval e1) then (if (isval e2) then (calc bop e1 e2) else Binop(bop, e1, step e2)) else Binop(bop, step e1, e2)
 | Let(x, e1, e2) -> if(isval e1) then (subst x e1 e2) else Let(x, step e1, e2)
 | If(e, e1, e2) -> if(e=Boolean(true)) then e1 else (if(e=Boolean(false)) then e2 else If(step e, e1, e2))
+| Fun _ -> raise CannotStep
+| App(e1, e2) -> 
+| Nil -> raise CannotStep
+| Cons(e1, e2) ->
+| Match(e1, e2, x, y, e3) -> 
 | _ -> raise CannotStep
 
 
