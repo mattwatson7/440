4dad6d21e047715c5b08bbdd50995b8a28868a83
Matthew Watson <113383737+mattwatson7@users.noreply.github.com>
2024-10-15 14:41:46 -0500

    Update ast.ml

--- hw3/hw3-src/src/ast.ml
+++ hw3/hw3-src/src/ast.ml
@@ -3,5 +3,3 @@
 type bop = Plus | Times | Leq | And | Or | Minus | Div | Geq | Le | Ge | Eq 
 
 type expr = Var of string | Integer of int | Boolean of bool | Binop of ( bop * expr * expr)|  If of (expr * expr * expr) | Let of ( string * expr * expr) |  Fun of ( string * expr) | App of (expr * expr) | Nil | Cons of (expr * expr) | Match of (expr * expr * string * string * expr)
-
-type t = Int of int | Bool of bool | List of t | Arrow of (t * t)
