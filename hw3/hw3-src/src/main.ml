689ad19d25934fb06c4156241661fa4030af9371
Matthew Watson <113383737+mattwatson7@users.noreply.github.com>
2024-10-15 23:06:50 -0500

    Update main.ml

--- hw3/hw3-src/src/main.ml
+++ hw3/hw3-src/src/main.ml
@@ -61,7 +61,7 @@ let rec step (w:expr) : expr = match w with
 | Nil -> Nil
 | Cons(e1, e2) -> if(isval e1) then (if(isval e2) then Cons(e1, e2) else Cons(e1, step e2)) else Cons(step e1, e2)
 | Match(e1, e2, x, y, e3) -> match e1 with
-       | Nil -> (step e2) 
+       | Nil -> e2 
        | Cons(h,t) -> (subst y t (subst x h e3)) 
        | _ -> Match(step e1, e2, x, y, e3)
 
